842e5700f44667bfaff6cab33c6833300703b71f
AbiyaY <abiyay30@gmail.com>
2021-10-28 20:10:23 +0100

    Completed Outline

--- script.js
+++ script.js
@@ -1,12 +1,55 @@
 //You can edit ALL of the code here
+const rootElem = document.getElementById("root");
+const divContainerElem = document.createElement("div");
+divContainerElem.id = "div-container";
+rootElem.appendChild(divContainerElem);
+
 function setup() {
   const allEpisodes = getAllEpisodes();
   makePageForEpisodes(allEpisodes);
 }
 
 function makePageForEpisodes(episodeList) {
-  const rootElem = document.getElementById("root");
-  rootElem.textContent = `Got ${episodeList.length} episode(s)`;
+  
+  episodeList.forEach(episode => {
+
+    const divCardElem = document.createElement("div");
+    divCardElem.setAttribute("id", "divCard");
+    const divTitleElem = document.createElement("div");
+    divTitleElem.setAttribute("id", "divTitle");
+    const divImgElem = document.createElement("div");
+    divImgElem.setAttribute("id", "divImg");
+    const divSummaryElem = document.createElement("div");
+    divSummaryElem.setAttribute("id", "divSummary");
+
+    const h1Elem = document.createElement("h1");
+    if(episode.number < 10) {
+       h1Elem.innerHTML = `${episode.name} - S0${episode.season}E0${episode.number}`;
+    } else {
+      h1Elem.innerHTML = `${episode.name} - S0${episode.season}E${episode.number}`;
+    }
+   
+    //pElem.innerHTML = ``;
+    divTitleElem.appendChild(h1Elem);
+
+    const imageElem = document.createElement("img");
+    imageElem.setAttribute("class", "image");
+    imageElem.src = `${episode.image.medium}`;
+    divImgElem.appendChild(imageElem);
+
+    const p1Elem = document.createElement("p");
+    p1Elem.innerHTML = `${episode.summary}`;
+    divSummaryElem.appendChild(p1Elem);
+
+    divCardElem.appendChild(divTitleElem);
+    divCardElem.appendChild(divImgElem);
+    divCardElem.appendChild(divSummaryElem);
+    divContainerElem.appendChild(divCardElem);
+  });
+ 
+  console.log(episodeList);
+
+  //rootElem.innerHTML = `Got ${episodeList.name} episode(s)`;
 }
 
 window.onload = setup;
