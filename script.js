cbe36e3e9740127ceb49cbf759fe803b63235807
AbiyaY <abiyay30@gmail.com>
2021-11-10 11:33:14 +0000

    Upto level 350 Completed

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
 //You can edit ALL of the code here
-const allEpisodes = getAllEpisodes();
+//const allEpisodes = getAllEpisodes();
 const rootElem = document.getElementById("root");
 
 const divSelectElem = document.createElement("div");
 rootElem.appendChild(divSelectElem);
 const selectElem = document.createElement("select");
 selectElem.setAttribute("id", "select");
+//selectElem.setAttribute("placeholder", "Search");
 divSelectElem.appendChild(selectElem);
 //const optionElem = document.createElement("option");
 //selectElem.appendChild(optionElem);
@@ -20,6 +21,10 @@ searchElem.setAttribute("name", "search");
 searchElem.setAttribute("placeholder", "Search");
 divSearchElem.appendChild(searchElem);
 
+const divDisplay = document.createElement("div");
+divDisplay.setAttribute("id", "display");
+rootElem.appendChild(divDisplay);
+
 // const buttonElem = document.createElement("button");
 // buttonElem.setAttribute("class", "search");
 // buttonElem.innerHTML = "Search";
@@ -31,14 +36,32 @@ rootElem.appendChild(divContainerElem);
 
 
 
+const h4El = document.createElement("h4");
+ 
+  divDisplay.appendChild(h4El);
+
 
 
 function setup() {  
-  makePageForEpisodes(allEpisodes);
+  let allEpisodes = fetch("https://api.tvmaze.com/shows/82/episodes")
+    .then(response => response.json())
+    .then(data => {
+        allEpisodes = data;
+        console.log(data);
+        makePageForEpisodes(allEpisodes);
+        selectEpisodes(allEpisodes);
+        searchEpisodes(allEpisodes);
+    })
+    .catch(err => console.log(err));
+    //makePageForEpisodes(allEpisodes);
+    
+    
 }
 
 
 function makePageForEpisodes(episodeList) {
+
+   h4El.textContent = `Displaying ${episodeList.length} episodes`;
   
   episodeList.forEach(episode => {
 
@@ -70,6 +93,8 @@ function makePageForEpisodes(episodeList) {
       h1Elem.innerHTML = `${episode.name} - S0${episode.season}E${episode.number}`;
     }
    
+    
+
     //pElem.innerHTML = ``;
     divTitleElem.appendChild(h1Elem);
 
@@ -101,22 +126,31 @@ function makePageForEpisodes(episodeList) {
   // });
 
   //rootElem.innerHTML = `Got ${episodeList.name} episode(s)` 
+  
+
+
 
 }
 
-selectElem.addEventListener("click", (e) => {
+function selectEpisodes(episodeList) {
+
+    selectElem.addEventListener("click", (e) => {
     
     let selectItem = e.target.value;
-    let selectFilter = allEpisodes.filter(episode => {
+    let selectFilter = episodeList.filter(episode => {
       return episode.name === selectItem;
     });
     console.log(selectFilter);
      divContainerElem.innerHTML = "";
   makePageForEpisodes(selectFilter);
+  console.log(selectFilter);
+  h4El.textContent = `Displaying ${selectFilter.length}/${episodeList.length} episodes`;
+  
 });
+}
 
-
-searchElem.addEventListener("input", e => {
+function searchEpisodes(allEpisodes) {
+    searchElem.addEventListener("input", e => {
     const searchItem = e.target.value.toLowerCase();
     const searchFilter = allEpisodes.filter(episodes => {
       return episodes.name.toLowerCase().includes(searchItem) || episodes.summary.toLowerCase().includes(searchItem);
@@ -124,6 +158,11 @@ searchElem.addEventListener("input", e => {
     console.log(searchFilter);
     divContainerElem.innerHTML = "";
   makePageForEpisodes(searchFilter);
+  console.log(allEpisodes.length);
+  h4El.textContent = `Displaying ${searchFilter.length}/${allEpisodes.length} episodes`;
 });
+}
+//console.log(allEpisodes);
+
 
 window.onload = setup;
